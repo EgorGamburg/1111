4041503b613efc08be64dd50e229119e7e24f43e
egor <egor.gamburg.igorevich@gmail.com>
2024-10-01 18:22:46 +0300

    Init

--- main.py
+++ main.py
@@ -6,25 +6,34 @@ from samurai import Samurai
 from ninja import Ninja
 from vampyre import Vampyre
 
-player1 = Paladin("Kakyoin", 100, 5, 1)
-player2 = Berserk("Avdol", 200, 15, 5)
-player3 = Assassin("Diavolo", 80, 10, 2)
-player4 = Samurai("Polnareff", 150, 15, 3)
-player5 = Ninja("Ryu", 90, 12, 1)
-player6 = Vampyre("Dio", 120, 20, 5)
+players_list = []
+
+player_1_name = input('Введіть ім\'я першого гравця: ')
+player_1_class = input('Введіть клас: ').lower()
+
+if player_1_class == 'paladin':
+    player1 = Paladin(player_1_name, 100, 15, 1)
+elif player_1_class == 'berserk':
+    player1 = Berserk(player_1_name, 150, 20, 2)
+elif player_1_class == 'assassin':
+    player1 = Assassin(player_1_name, 120, 10, 1)
+elif player_1_class == 'samurai':
+    player1 = Samurai(player_1_name, 150, 15, 3)
+elif player_1_class == 'ninja':
+    player1 = Ninja(player_1_name, 100, 20, 2)
+elif player_1_class == 'vampyre':
+    player1 = Vampyre(player_1_name, 120, 20, 3)
+else:
+    player1 = Character(player_1_name, 100, 10, 0)
 
 print(player1)
-print(player2)
-print(player3)
-print(player4)
-print(player5)
-print(player6)
+players_list.append(player1)
 
-player1.attack(player2)
-player2.attack(player1)
-player3.attack(player4)
-player4.attack(player3)
-player5.attack(player6)
-player6.attack(player5)
+player2 = Paladin('Kakyoin', 100, 15, 1)
+player2.show_stats()
 
-print(f"\n{player1}\n{player2}\n{player3}\n{player4}\n{player5}\n{player6}\n")
+while player1.health > 0 and player2.health > 0:
+    player1.attack(player2)
+    player2.attack(player1)
+
+print(f'\n{player1}\n{player2}\n')
